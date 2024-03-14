c0b97a25e65ff9f6a9afc634dd8df506039799c2
Macailh <salvadorenriquegb@gmail.com>
2024-03-13 21:29:19 -0600

    feat: add FastAPI base for AWS Lambda deployment

--- es_notifier/es_notifier_stack.py
+++ es_notifier/es_notifier_stack.py
@@ -1,19 +1,17 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Stack
+from aws_cdk import aws_lambda as _lambda
 from constructs import Construct
+
 
 class EsNotifierStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "EsNotifierQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        es_notifier_api_function = _lambda.Function(
+            self,
+            "EsNotifierApiFunction",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="lambda_handler.main",
+            code=_lambda.Code.from_asset("es_notifier/es_notifier_api_function"),
+        )
