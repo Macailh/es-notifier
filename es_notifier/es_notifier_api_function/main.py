1e4d4bd53c98a72c480d5856838bffb8ef0f0b58
Macailh <salvadorenriquegb@gmail.com>
2024-03-19 20:56:11 -0600

    Add handler with mangum for lambda

--- es_notifier/es_notifier_api_function/main.py
+++ es_notifier/es_notifier_api_function/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from routers import notifier_router
+from mangum import Mangum
 
 app = FastAPI()
 
 app.include_router(notifier_router.router, prefix="/api/v1")
+
+
+handler = Mangum(app)
