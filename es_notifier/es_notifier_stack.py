ca966cf33962dbb3cb608947d12b064ff0f256c6
Macailh <salvadorenriquegb@gmail.com>
2024-03-19 20:56:40 -0600

    Update api gateway structure and exclude unused files

--- es_notifier/es_notifier_stack.py
+++ es_notifier/es_notifier_stack.py
@@ -10,13 +10,17 @@ class EsNotifierStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        exclude_files = ["__pycache__", "test/", "local.py"]
+
         es_notifier_api_function = _lambda.Function(
             self,
             "EsNotifierApiFunction",
             function_name="esNotifierApiFunction",
             runtime=_lambda.Runtime.PYTHON_3_12,
-            handler="lambda_handler.main",
-            code=_lambda.Code.from_asset("es_notifier/es_notifier_api_function"),
+            handler="main.handler",
+            code=_lambda.Code.from_asset(
+                "es_notifier/es_notifier_api_function", exclude=exclude_files
+            ),
         )
 
         es_notifier_api_gateway = apigateway.LambdaRestApi(
@@ -24,9 +28,14 @@ class EsNotifierStack(Stack):
             "EsNotifierApiGateway",
             rest_api_name="esNotifierApiGateway",
             handler=es_notifier_api_function,
+            proxy=False,
         )
 
-        # es_notifier_api_function.add_environment("API_URL", es_notifier_api_gateway.url)
+        root_resource = es_notifier_api_gateway.root
+        api_resource = root_resource.add_resource("api")
+        v1_resource = api_resource.add_resource("v1")
+        proxy_resource = v1_resource.add_proxy(any_method=True)
+
         # Create a CloudFormation Custom Resource to update the Lambda function after creation
         api_url_updater = crs.AwsCustomResource(
             self,
